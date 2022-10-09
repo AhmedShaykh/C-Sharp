8f32b96647d5ee92c13801619b82068ac3f18d56
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-10-09 23:36:21 +0500

    Git Init

---
+++ program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace runCode {
+    class firstC {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
